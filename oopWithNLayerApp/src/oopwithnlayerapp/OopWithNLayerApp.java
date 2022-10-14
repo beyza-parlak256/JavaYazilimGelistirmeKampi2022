5fbb2cbd65633fea85e7331fdfa718737f472f05
beyza-parlak256 <beyza.parlak98@erzurum.edu.tr>
2022-10-14 23:22:44 +0300

    Package Added, 2 Class Created

--- oopWithNLayerApp/src/oopwithnlayerapp/OopWithNLayerApp.java
+++
@@ -1,7 +0,0 @@
-package oopwithnlayerapp;
-public class OopWithNLayerApp {
-    public static void main(String[] args) {
-        
-    }
-    
-}
---
+++ oopWithNLayeredApp/src/Main.java
@@ -0,0 +1,6 @@
+package oopWithNLayeredApp;
+public class Main {
+    public static void main(String[] args){
+        
+    }
+}
